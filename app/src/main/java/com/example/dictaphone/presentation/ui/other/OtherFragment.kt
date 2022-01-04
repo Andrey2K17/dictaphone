c281920a73fe2e77e718927ff4c0386227ee7f60
AndreyG <nba.grinin.96@gmail.com>
2022-01-04 14:34:49 +0700

    создание фрагментов для меню

---
+++ app/src/main/java/com/example/dictaphone/presentation/ui/other/OtherFragment.kt
@@ -0,0 +1,8 @@
+package com.example.dictaphone.presentation.ui.other
+
+import com.example.dictaphone.R
+import com.example.dictaphone.presentation.ui.base.BaseFragment
+
+class OtherFragment : BaseFragment() {
+    override val layoutRes = R.layout.fragment_other
+}
