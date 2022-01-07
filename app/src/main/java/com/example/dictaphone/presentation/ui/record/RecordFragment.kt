7919f2c3cb82bafbe95a50fe4fd124c6c9b667fc
AndreyG <nba.grinin.96@gmail.com>
2022-01-07 11:34:40 +0700

    fix FragmentRecord

--- app/src/main/java/com/example/dictaphone/presentation/ui/record/RecordFragment.kt
+++ app/src/main/java/com/example/dictaphone/presentation/ui/record/RecordFragment.kt
@@ -1,6 +1,264 @@
 package com.example.dictaphone.presentation.ui.record
 
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.media.MediaRecorder
+import android.os.*
+import android.util.Log
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.LinearLayout
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
+import com.example.dictaphone.App
 import com.example.dictaphone.R
+import com.example.dictaphone.common.Timer
+import com.example.dictaphone.db.AppDatabase
+import com.example.dictaphone.db.AudioRecord
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import kotlinx.android.synthetic.main.bottom_sheet.*
+import kotlinx.android.synthetic.main.fragment_record.*
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+import java.io.ObjectOutputStream
+import java.text.SimpleDateFormat
+import java.util.*
 
-class RecordFragment : Fragment(R.layout.fragment_record)
+const val REQUEST_CODE = 200
+
+class RecordFragment : Fragment(R.layout.fragment_record), Timer.OnTimerTickListener {
+
+    private lateinit var recorder: MediaRecorder
+    private var permissionGranted = false
+    private var permissions = arrayOf(Manifest.permission.RECORD_AUDIO)
+    private var dirPath = ""
+    private var fileName = ""
+    private var isRecording = false
+    private var isPaused = false
+
+    private var duration = ""
+
+    private lateinit var vibrator: Vibrator
+
+    private lateinit var timer: Timer
+
+    private lateinit var db: AppDatabase
+
+    private lateinit var bottomSheetBehavior: BottomSheetBehavior<LinearLayout>
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        permissionGranted = ActivityCompat.checkSelfPermission(
+            view.context,
+            permissions[0]
+        ) == PackageManager.PERMISSION_GRANTED
+        if (!permissionGranted) {
+            ActivityCompat.requestPermissions(requireActivity(), permissions, REQUEST_CODE)
+        }
+
+        db = App.database
+        fetchRecords()
+
+        bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet)
+        bottomSheetBehavior.peekHeight = 0
+        bottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
+
+        timer = Timer(this)
+        vibrator = activity?.getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
+
+        btnRecord.setOnClickListener {
+            when {
+                isPaused -> resumeRecording()
+                isRecording -> pauseRecording()
+                else -> startRecording()
+            }
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                vibrator.vibrate(
+                    VibrationEffect.createOneShot(
+                        50,
+                        VibrationEffect.DEFAULT_AMPLITUDE
+                    )
+                )
+            }
+        }
+        btnList.setOnClickListener {
+            Toast.makeText(view.context, "Список", Toast.LENGTH_SHORT).show()
+        }
+        btnDone.setOnClickListener {
+            stopRecording()
+            Toast.makeText(view.context, "Запись сохранена", Toast.LENGTH_SHORT).show()
+
+            bottomSheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
+            bottomSheetBG.visibility = View.VISIBLE
+            fileNameInput.setText(fileName)
+        }
+
+        btnCancel.setOnClickListener {
+            File("$dirPath$fileName.mp3").delete()
+            dismiss()
+        }
+
+        btnOK.setOnClickListener {
+            dismiss()
+            save()
+        }
+
+        bottomSheetBG.setOnClickListener {
+            File("$dirPath$fileName.mp3").delete()
+            dismiss()
+        }
+
+        btnDelete.setOnClickListener {
+            stopRecording()
+            File("$dirPath$fileName.mp3").delete()
+            Toast.makeText(view.context, "Запись удалена", Toast.LENGTH_SHORT).show()
+        }
+
+        btnDelete.isClickable = false
+
+
+    }
+
+    private fun save() {
+        val newFileName = fileNameInput.text.toString()
+        if (newFileName != fileName) {
+            var newFile = File("$dirPath$newFileName.mp3")
+            File("$dirPath$fileName.mp3").renameTo(newFile)
+        }
+
+        var filePath = "$dirPath$newFileName.mp3"
+        var timesTamp = Date().time
+        var ampsPath = "$dirPath$newFileName"
+
+        try {
+            var fos = FileOutputStream(ampsPath)
+            var out = ObjectOutputStream(fos)
+            //out.writeObject(amplitudes)
+            fos.close()
+            out.close()
+        } catch (e: IOException) {
+        }
+
+        var record = AudioRecord(newFileName, filePath, timesTamp, duration, ampsPath)
+        GlobalScope.launch {
+            db.audioRecordDao().insert(record)
+        }
+    }
+
+    private fun dismiss() {
+        bottomSheetBG.visibility = View.GONE
+        hideKeyboard(fileNameInput)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            bottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
+        }, 100)
+    }
+
+    private fun hideKeyboard(view: View) {
+        val imm = activity?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
+    }
+
+    private fun pauseRecording() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            recorder.pause()
+        }
+        isPaused = true
+        btnRecord.setImageResource(R.drawable.ic_record)
+        timer.pause()
+    }
+
+    private fun resumeRecording() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            recorder.resume()
+        }
+        isPaused = false
+        btnRecord.setImageResource(R.drawable.ic_pause)
+        timer.start()
+    }
+
+    private fun fetchRecords() {
+        GlobalScope.launch {
+            var queryResult = db.audioRecordDao().getAll()
+            Log.d("Rec123", queryResult.toString())
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_CODE)
+            permissionGranted = grantResults[0] == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun startRecording() {
+        if (!permissionGranted) {
+            ActivityCompat.requestPermissions(requireActivity(), permissions, REQUEST_CODE)
+            return
+        }
+
+        recorder = MediaRecorder()
+        dirPath = "${activity?.externalCacheDir?.absolutePath}/"
+
+        var simpleDateFormat = SimpleDateFormat("yyyy.MM.DD_hh.mm.ss")
+        var date = simpleDateFormat.format(Date())
+        fileName = "audio_record_$date"
+        recorder.apply {
+            setAudioSource(MediaRecorder.AudioSource.MIC)
+            setOutputFormat(MediaRecorder.OutputFormat.MPEG_4)
+            setAudioEncoder(MediaRecorder.AudioEncoder.AAC)
+            setOutputFile("$dirPath$fileName.mp3")
+
+            try {
+                prepare()
+            } catch (e: IOException) {
+            }
+
+            start()
+        }
+
+        btnRecord.setImageResource(R.drawable.ic_pause)
+        isRecording = true
+        isPaused = false
+
+        timer.start()
+
+        btnDelete.isClickable = true
+        btnDelete.setImageResource(R.drawable.ic_delete)
+
+        btnList.visibility = View.GONE
+        btnDone.visibility = View.VISIBLE
+    }
+
+    private fun stopRecording() {
+        timer.stop()
+
+        recorder.apply {
+            stop()
+            release()
+        }
+        isPaused = false
+        isRecording = false
+
+        btnList.visibility = View.VISIBLE
+        btnDone.visibility = View.GONE
+        btnDelete.isClickable = false
+        btnDelete.setImageResource(R.drawable.ic_delete_disabled)
+        btnRecord.setImageResource(R.drawable.ic_record)
+        tvTimer.text = "00:00.0"
+    }
+
+    override fun onTimerTick(duration: String) {
+        tvTimer.text = duration
+        this.duration = duration.dropLast(3)
+    }
+}
